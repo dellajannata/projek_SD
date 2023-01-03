2df70d385e0ad0430a6531a3bd1683da04fd6ee3
rsakml <rossaakmalia66@gmail.com>
2023-01-03 22:09:42 +0700

    update tentang kami

--- resources/views/tentang/index.blade.php
+++ resources/views/tentang/index.blade.php
@@ -27,6 +27,7 @@
                                         <th scope="col">Judul</th>
                                         <th scope="col">Deskripsi</th>
                                         <th scope="col">Gambar</th>
+                                        <th scope="col">Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
