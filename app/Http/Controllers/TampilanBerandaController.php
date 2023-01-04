772fd91e647abf3d61f5eb87a3db7d261d732851
rsakml <rossaakmalia66@gmail.com>
2023-01-04 11:25:28 +0700

    beranda kegiatan pembiasaan

--- app/Http/Controllers/TampilanBerandaController.php
+++ app/Http/Controllers/TampilanBerandaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Informasi;
 use App\Models\informasiLomba;
+use App\Models\Pembiasaan;
 
 class TampilanBerandaController extends Controller
 {
@@ -12,6 +13,7 @@ class TampilanBerandaController extends Controller
     {
         $informasisd = Informasi::orderBy('id', 'asc')->paginate(3);
         $informasiLomba = InformasiLomba::orderBy('id', 'asc')->paginate(3);
-        return view('layouts.index', compact('informasisd', 'informasiLomba'));
+        $pembiasaan = Pembiasaan::orderBy('id', 'asc')->paginate(8);
+        return view('layouts.index', compact('informasisd', 'informasiLomba', 'pembiasaan'));
     }
 }
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -118,20 +118,19 @@
                             <div class="tab-pane active fade show" id="tab1">
                                 <div class="row text-center">
                                     @foreach ($informasisd as $i)
-                                    <div class="col-md-4">
-                                        <div class="pricing-table pricing-table-highlighted">
-                                            <div class="pricing-table-features">
-                                                <img src="{{ asset('storage/informasi/' . $i->image) }}" style="width: 100%;
+                                        <div class="col-md-4">
+                                            <div class="pricing-table pricing-table-highlighted">
+                                                <div class="pricing-table-features">
+                                                    <img src="{{ asset('storage/informasi/' . $i->image) }}"
+                                                        style="width: 100%;
                                                 height: 100%;">
-                                            </div>
-                                            <div class="pricing-table-space"></div>
-                                            <div class="pricing-table-header grd1">
-                                                {{-- <h2>$45</h2>
-                                                <h3>per month</h3> --}}
-                                                <h3 class="title">{{ $i->deskripsi }}</h3>
+                                                </div>
+                                                <div class="pricing-table-space"></div>
+                                                <div class="pricing-table-header grd1">
+                                                    <h3 class="title">{{ $i->deskripsi }}</h3>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
                                     @endforeach
                                 </div><!-- end row -->
                             </div><!-- end pane -->
@@ -139,159 +138,53 @@
                             <div class="tab-pane fade" id="tab2">
                                 <div class="row text-center">
                                     @foreach ($informasiLomba as $i)
-                                    <div class="col-md-4">
-                                        <div class="pricing-table pricing-table-highlighted">
-                                            <div class="pricing-table-features">
-                                                <img src="{{ asset('storage/informasiLomba/' . $i->image) }}" style="width: 100%;
+                                        <div class="col-md-4">
+                                            <div class="pricing-table pricing-table-highlighted">
+                                                <div class="pricing-table-features">
+                                                    <img src="{{ asset('storage/informasiLomba/' . $i->image) }}"
+                                                        style="width: 100%;
                                                 height: 100%;">
-                                            </div>
-                                            <div class="pricing-table-space"></div>
-                                            <div class="pricing-table-header grd1">
-                                                {{-- <h2>$45</h2>
-                                                <h3>per month</h3> --}}
-                                                <h3 class="title">{{ $i->deskripsi }}</h3>
+                                                </div>
+                                                <div class="pricing-table-space"></div>
+                                                <div class="pricing-table-header grd1">
+                                                    <h3 class="title">{{ $i->deskripsi }}</h3>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
                                     @endforeach
                                 </div><!-- end row -->
                             </div><!-- end pane -->
                         </div><!-- end content -->
                     </div><!-- end col -->
                 </div><!-- end row -->
-                <br><br>
-
-                <div class="row align-items-center">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                        <div class="message-box">
-                            <h4>2018 BEST SmartEDU education school</h4>
-                            <h2>Welcome to SmartEDU education school</h2>
-                            <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam
-                                risus. Sed a tellus quis mi rhoncus dignissim.</p>
-
-                            <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus
-                                bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis
-                                montes, nascetur ridiculus mus. Sed vitae rutrum neque. </p>
-
-                            <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
-                        </div><!-- end messagebox -->
-                    </div><!-- end col -->
-
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                        <div class="post-media wow fadeIn">
-                            <img src="images/about_02.jpg" alt="" class="img-fluid img-rounded">
-                        </div><!-- end media -->
-                    </div><!-- end col -->
-                </div>
-                <div class="row align-items-center">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                        <div class="post-media wow fadeIn">
-                            <img src="images/about_03.jpg" alt="" class="img-fluid img-rounded">
-                        </div><!-- end media -->
-                    </div><!-- end col -->
-
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                        <div class="message-box">
-                            <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                            <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus
-                                bibendum.</p>
-
-                            <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
-                        </div><!-- end messagebox -->
-                    </div><!-- end col -->
-
-                </div><!-- end row -->
-            </div><!-- end container -->
-        </div><!-- end section -->
-
+            </div>
+        </div>
+        <br><br><br>
         <section class="section lb page-section">
             <div class="container">
                 <div class="section-title row text-center">
                     <div class="col-md-8 offset-md-2">
-                        <h3>Our history</h3>
-                        <p class="lead">Lorem Ipsum dolroin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
-                            lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem!</p>
+                        <h3>Kegiatan Pembiasaan</h3>
                     </div>
                 </div><!-- end title -->
-                <div class="timeline">
-                    <div class="timeline__wrap">
-                        <div class="timeline__items">
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-01">
-                                    <h2>2018</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div class="row">
+                    @foreach ($pembiasaan as $p)
+                        <div class="col-lg-3 col-md-6 col-12">
+                            <div class="our-team">
+                                <div class="team-img">
+                                    <img src="{{ asset('storage/pembiasaan/' . $p->image) }}">
                                 </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-02">
-                                    <h2>2015</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-03">
-                                    <h2>2014</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-04">
-                                    <h2>2012</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-01">
-                                    <h2>2010</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-02">
-                                    <h2>2007</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-03">
-                                    <h2>2004</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="timeline__item">
-                                <div class="timeline__content img-bg-04">
-                                    <h2>2002</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim neque
-                                        condimentum lacus dapibus. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <div class="team-content">
+                                    <h3 class="title">{{ $p->namaKegiatan }}</h3>
+                                    <p class="post">{{ $p->deskripsi }}</p>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
-        </section>
-
+        </section><!-- end section -->
+        
         <div class="section cl">
             <div class="container">
                 <div class="row text-left stat-wrap">
@@ -323,7 +216,8 @@
             <div class="container">
                 <div class="section-title text-center">
                     <h3>Choose Your Plan</h3>
-                    <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore et
+                    <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore
+                        et
                         dolore magna aliqua. </p>
                 </div><!-- end title -->
 
@@ -337,7 +231,8 @@
             <div class="container">
                 <div class="section-title text-center">
                     <h3>Testimonials</h3>
-                    <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore et
+                    <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore
+                        et
                         dolore magna aliqua. </p>
                 </div><!-- end title -->
 
@@ -365,7 +260,8 @@
                                 </div>
                                 <div class="desc">
                                     <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                    <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and
+                                    <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure
+                                        and
                                         praising pain was born and I will give you completed.</p>
                                 </div>
                                 <!-- end testi-meta -->
@@ -379,7 +275,8 @@
                                 </div>
                                 <div class="desc">
                                     <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                    <p class="lead">The master-builder of human happines no one rejects, dislikes avoids
+                                    <p class="lead">The master-builder of human happines no one rejects, dislikes
+                                        avoids
                                         pleasure itself, because it is very pursue pleasure. </p>
                                 </div>
                                 <!-- end testi-meta -->
@@ -406,7 +303,8 @@
                                 </div>
                                 <div class="desc">
                                     <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                    <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and
+                                    <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure
+                                        and
                                         praising pain was born and I will give you completed.</p>
                                 </div>
                                 <!-- end testi-meta -->
@@ -420,7 +318,8 @@
                                 </div>
                                 <div class="desc">
                                     <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                    <p class="lead">The master-builder of human happines no one rejects, dislikes avoids
+                                    <p class="lead">The master-builder of human happines no one rejects, dislikes
+                                        avoids
                                         pleasure itself, because it is very pursue pleasure. </p>
                                 </div>
                                 <!-- end testi-meta -->
