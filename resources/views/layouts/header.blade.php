1a277ea3aca7c7e6ee070a352747dd3fe47c918a
= <LiaAufarrahman09@gmail.com>
2023-01-06 19:47:42 +0700

    Update logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -90,8 +90,8 @@
 	<header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="index.html">
-					<img src="images/logo (1).png" alt="" />
+				<a class="navbar-brand" href="beranda">
+					<img src="images/logo.png" alt="" / style="max-width:70px;max-height:70px;float:left;" >
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="icon-bar"></span>
