717989a18eb164136bb83ba73ce92a84e8eefae2
rsakml <rossaakmalia66@gmail.com>
2023-01-06 08:37:07 +0700

    update layout tentang

--- resources/views/layouts/tentang.blade.php
+++ resources/views/layouts/tentang.blade.php
@@ -1,95 +1,77 @@
 @extends('layouts.main')
 @section('layouts.content')
-	<div class="all-title-box">
-		<div class="container text-center">
-			<h1>Tentang Kami<span class="m_1">UPT SD Negeri 108 Gresik</span></h1>
-		</div>
-	</div>
-	
+    <div class="all-title-box">
+        <div class="container text-center">
+            <h1>Tentang Kami<span class="m_1">UPT SD Negeri 108 Gresik</span></h1>
+        </div>
+    </div>
+
     <div id="overviews" class="section lb">
         <div class="container">
             <div class="section-title row text-center">
                 <div class="col-md-8 offset-md-2">
                     <h3>UPT SD Negeri 108 Gresik</h3>
-                    <p class="lead">Salah satu satuan pendidikan dengan jenjang SD di Kedungsekar, Kec. Benjeng, Kab. Gresik, Jawa Timur. Yang berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.
+                    <p class="lead">Salah satu satuan pendidikan dengan jenjang SD di Kedungsekar, Kec. Benjeng, Kab.
+                        Gresik, Jawa Timur. Yang berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.
                     </p>
                 </div>
             </div><!-- end title -->
-        
+
             <div class="row align-items-center">
                 @foreach ($tentang as $tentangkami)
-                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                    <div class="message-box">
-                        <h4 class="title">{{ $tentangkami->judul }}</h4>
-                        <p class="post">{{ $tentangkami->deskripsi }}</p>
-                        <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
-                    </div><!-- end messagebox -->
-                </div><!-- end col -->
-				
-				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                    <div class="post-media wow fadeIn">
-                        <img src="{{ asset('storage/tentangkami/' . $tentangkami->gambar) }}" alt="" class="img-fluid img-rounded">
-                    </div><!-- end media -->
-                </div><!-- end col -->
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                        <div class="message-box">
+                            <h4 class="title">{{ $tentangkami->judul }}</h4>
+                            <p class="post">{{ $tentangkami->deskripsi }}</p>
+                            <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+                        </div><!-- end messagebox -->
+                    </div><!-- end col -->
+
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                        <div class="post-media wow fadeIn">
+                            <img src="{{ asset('storage/tentangkami/' . $tentangkami->gambar) }}" alt=""
+                                class="img-fluid img-rounded">
+                        </div><!-- end media -->
+                    </div><!-- end col -->
                 @endforeach
-			</div>
-			<div class="row align-items-center">
+            </div>
+            <div class="row align-items-center">
                 @foreach ($tentang as $tentangkami)
-				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                    <div class="post-media wow fadeIn">
-                        <img src="{{ asset('storage/tentangkami/' . $tentangkami->gambar) }}" alt="" class="img-fluid img-rounded">
-                    </div><!-- end media -->
-                </div><!-- end col -->
-				
-				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                    <div class="message-box">
-                        <h2class="title">{{ $tentangkami->judul }}</h2>
-                        <p class="post">{{ $tentangkami->deskripsi }}</p>
-                        <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
-                    </div><!-- end messagebox -->
-                </div><!-- end col -->
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                        <div class="post-media wow fadeIn">
+                            <img src="{{ asset('storage/tentangkami/' . $tentangkami->gambar) }}" alt=""
+                                class="img-fluid img-rounded">
+                        </div><!-- end media -->
+                    </div><!-- end col -->
+
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                        <div class="message-box">
+                            <h2class="title">{{ $tentangkami->judul }}</h2>
+                                <p class="post">{{ $tentangkami->deskripsi }}</p>
+                                <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+                        </div><!-- end messagebox -->
+                    </div><!-- end col -->
                 @endforeach
             </div><!-- end row -->
         </div><!-- end container -->
         {{ $tentang->links() }}
     </div><!-- end section -->
-	
-	<div class="hmv-box">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="inner-hmv">
-						<div class="icon-box-hmv"><i class="flaticon-achievement"></i></div>
-						<h3>Misi</h3>
-						<div class="tr-pa">M</div>
-						<p>1. Menerapkan pembelajaran yang menyenangkan dan bermakna yang mampu memotivasi peserta didik untuk selalu belajar dan menemukan pembelajaran. <br>
-                        2. Meningkatkan akhlak mulia peserta didik melalui tentang intrakurikuler, kokurikuler, dan ekstra kurikuker yang berakar pada agama, budaya, norma susila, dan tata krama yang berlaku di masyarakat.<br>
-                        3. Meningkatkan prestasi belajar peserta didik pada dimensi sikap, pengetahuan, dan keterampilan melalui tentang pembelajaran yang bermakna.<br> 
-                        4. Mengembangkan pendidikan kecakapan hidup melalui tentang pembiasaan/budaya sekolah.<br>
-                        5. Menerapkan pendidikan yang berwawasan lingkungan.
-                            </p>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="inner-hmv">
-						<div class="icon-box-hmv"><i class="flaticon-eye"></i></div>
-						<h3>Visi</h3>
-						<div class="tr-pa">V</div>
-						<p>Terwujudnya pembelajar sepanjang hayat yang berakhlak mulia, berprestasi, terampil, dan berwawasan lingkungan.</p>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="inner-hmv">
-						<div class="icon-box-hmv"><i class="flaticon-history"></i></div>
-						<h3>History</h3>
-						<div class="tr-pa">H</div>
-						<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam eligendi expedita, provident cupiditate in excepturi.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
+    <div class="hmv-box">
+        <div class="container">
+            <div class="row">
+                @foreach ($visimisi as $vm)
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="inner-hmv">
+                        {{-- <div class="icon-box-hmv"><i class="flaticon-achievement"></i></div> --}}
+                        <h3 class="title">{{ $vm->judul }}</h3>
+                        <span class="post">{{ $vm->deskripsi }}</span>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 
     <div id="testimonials" class="parallax section db parallax-off" style="background-image:url('images/parallax_04.jpg');">
         <div class="container">
@@ -101,64 +83,69 @@
                 <div class="col-md-12 col-sm-12">
                     <div class="testi-carousel owl-carousel owl-theme">
                         <div class="testimonial clearfix">
-							<div class="testi-meta">
+                            <div class="testi-meta">
                                 <img src="images/khoiri.jpg" alt="" class="img-fluid">
                                 <h4>Mochamad Khoiri</h4>
                             </div>
                             <div class="desc">
                                 <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and
+                                    praising pain was born and I will give you completed.</p>
                             </div>
                             <!-- end testi-meta -->
                         </div>
                         <!-- end testimonial -->
 
                         <div class="testimonial clearfix">
-							<div class="testi-meta">
+                            <div class="testi-meta">
                                 <img src="images/lilik afnani.jpg" alt="" class="img-fluid ">
                                 <h4>Lilik Afnani</h4>
                             </div>
                             <div class="desc">
                                 <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids
+                                    pleasure itself, because it is very pursue pleasure. </p>
                             </div>
                             <!-- end testi-meta -->
                         </div>
                         <!-- end testimonial -->
                         <div class="testimonial clearfix">
-							<div class="testi-meta">
+                            <div class="testi-meta">
                                 <img src="images/novitasari.jpg" alt="" class="img-fluid">
                                 <h4>Novitasari</h4>
                             </div>
                             <div class="desc">
                                 <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
-                                <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+                                <p class="lead">They have got my project on time with the competition with a sed highly
+                                    skilled, and experienced & professional team.</p>
                             </div>
                             <!-- end testi-meta -->
                         </div>
                         <!-- end testimonial -->
 
                         <div class="testimonial clearfix">
-							<div class="testi-meta">
+                            <div class="testi-meta">
                                 <img src="images/SITI NAIMAH.jpg" alt="" class="img-fluid">
                                 <h4>Siti Naimah</h4>
                             </div>
                             <div class="desc">
                                 <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
-                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+                                <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and
+                                    praising pain was born and I will give you completed.</p>
                             </div>
                             <!-- end testi-meta -->
                         </div>
                         <!-- end testimonial -->
 
                         <div class="testimonial clearfix">
-							<div class="testi-meta">
+                            <div class="testi-meta">
                                 <img src="images/SIYAMU UMARO.jpg" alt="" class="img-fluid">
                                 <h4>Siyamu Umaro</h4>
                             </div>
                             <div class="desc">
                                 <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
-                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+                                <p class="lead">The master-builder of human happines no one rejects, dislikes avoids
+                                    pleasure itself, because it is very pursue pleasure. </p>
                             </div>
                             <!-- end testi-meta -->
                         </div><!-- end testimonial -->
@@ -192,4 +179,4 @@
             </div><!-- end row -->
         </div><!-- end container -->
     </div><!-- end section -->  --}}
-    @endsection
+@endsection
