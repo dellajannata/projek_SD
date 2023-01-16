869b93c8bf0e03f40ba0cb3b82d63e81d9c36c54
= <dellajannata2112@gmail.com>
2023-01-16 20:38:29 +0700

    Update Tampilan

--- resources/views/adminBeranda/header.blade.php
+++ resources/views/adminBeranda/header.blade.php
@@ -14,17 +14,6 @@
               <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                   <span class="mdi mdi-menu"></span>
               </button>
-              <div class="search-field d-none d-md-block">
-                  <form class="d-flex align-items-center h-100" action="#">
-                      <div class="input-group">
-                          <div class="input-group-prepend bg-transparent">
-                              <i class="input-group-text border-0 mdi mdi-magnify"></i>
-                          </div>
-                          <input type="text" class="form-control bg-transparent border-0"
-                              placeholder="Search projects">
-                      </div>
-                  </form>
-              </div>
               <ul class="navbar-nav navbar-nav-right">
                   <li class="nav-item nav-profile dropdown">
                       <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-bs-toggle="dropdown"
@@ -48,11 +37,6 @@
                               @csrf
                           </form>
                       </div>
-                  </li>
-                  <li class="nav-item d-none d-lg-block full-screen-link">
-                      <a class="nav-link">
-                          <i class="mdi mdi-fullscreen" id="fullscreen-button"></i>
-                      </a>
                   </li>
                   <li class="nav-item nav-settings d-none d-lg-block">
                       <a class="nav-link" href="#">
