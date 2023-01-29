28aff3fe7f99db43e9f53d64931310cc61ffc3d9
= <dellajannata2112@gmail.com>
2023-01-29 12:26:57 +0700

    Update Title

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SDN 108 Gresik</title>
+    <title>UPT SD Negeri 108 Gresik</title>
     <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" />
     <link rel="stylesheet" href="login.css">
 </head>
